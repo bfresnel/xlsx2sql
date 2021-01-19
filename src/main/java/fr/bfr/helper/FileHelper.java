eb096176dcd520c1c2d2066308a2c4358f80996b
bfresnel <benoit.fresnel.1@gmail.com>
2021-01-19 02:03:01 +0100

    Removed FileHelper<T>

--- src/main/java/fr/bfr/helper/FileHelper.java
+++
@@ -1,8 +0,0 @@
-package fr.bfr.helper;
-
-public class FileHelper<T> {
-
-    public static String writeSql() {
-        return "INSERT INTO T_SHIP(id,name,rarity,type,subtype,affiliation)";
-    }
-}
